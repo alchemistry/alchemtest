b07d8c7a20c3b4c0de792ddec2a12c9676b0db56
brycestx <bryce@silicontx.com>
2018-01-11 16:46:26 -0500

    Added Bace CAT-13d~CAT-17a perturbation with mbar from JACS set and load_bace_example function to access.py

--- src/alchemtest/amber/access.py
+++ src/alchemtest/amber/access.py
@@ -7,6 +7,35 @@ from glob import glob
 
 from .. import Bunch
 
+def load_bace_example():
+    """Load Amber Bace example perturbation.
+    Returns
+    -------
+    data: Bunch
+        Dictionary-like object, the interesting attributes are:
+
+        - 'data' : the data files files by system and alchemical leg
+
+    """
+    module_path = dirname(__file__)
+    data = {'complex':
+                {'decharge': glob(join(module_path, 'bace_CAT-13d~CAT-17a/complex/decharge/*/ti-*.out')),
+                 'recharge': glob(join(module_path, 'bace_CAT-13d~CAT-17a/complex/recharge/*/ti-*.out')),
+                 'vdw': glob(join(module_path, 'bace_CAT-13d~CAT-17a/complex/vdw/*/ti-*.out'))
+                 },
+            'solvated':
+                {'decharge': glob(join(module_path, 'bace_CAT-13d~CAT-17a/solvated/decharge/*/ti-*.out')),
+                 'recharge': glob(join(module_path, 'bace_CAT-13d~CAT-17a/solvated/recharge/*/ti-*.out')),
+                 'vdw': glob(join(module_path, 'bace_CAT-13d~CAT-17a/solvated/vdw/*/ti-*.out'))
+                 }
+            }
+
+    with open(join(module_path, 'bace_CAT-13d~CAT-17a', 'descr.rst')) as rst_file:
+        fdescr = rst_file.read()
+
+    return Bunch(data=data,
+                 DESCR=fdescr)
+
 def load_simplesolvated():
     """Load the Amber solvated dataset.
 
