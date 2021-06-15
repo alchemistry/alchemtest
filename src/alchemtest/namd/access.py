4b4ff20624e796e4a1eb2017c6d66fdb47144594
Oliver Beckstein <orbeckst@gmail.com>
2021-06-15 12:28:40 -0700

    fixed SyntaxError in NAMD IDWS data set accessor

--- src/alchemtest/namd/access.py
+++ src/alchemtest/namd/access.py
@@ -44,7 +44,7 @@ def load_idws():
     """
 
     module_path = dirname(__file__)
-    data = {'forward': [join(module_path, 'idws', 'quickidws.fepout.bz2']}
+    data = {'forward': [join(module_path, 'idws', 'quickidws.fepout.bz2')]}
 
     with open(join(module_path, 'idws', 'descr.rst')) as rst_file:
         fdescr = rst_file.read()
