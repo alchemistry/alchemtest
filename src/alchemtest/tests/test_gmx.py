c88370551bf2efcdc78de4d70243f71e36375c85
Zhiyi Wu <zhiyi.wu@bioch.ox.ac.uk>
2021-07-12 09:53:18 +0100

    Update test_gmx.py

--- src/alchemtest/tests/test_gmx.py
+++ src/alchemtest/tests/test_gmx.py
@@ -1,18 +1,33 @@
 '''Tests for all the gromacs dataset'''
+import os
 import pytest
 
 from alchemtest.gmx import load_benzene
 
 class Testbenzene():
-    @pytest.fixture(scope="class")
-    def bz(self):
-        return load_benzene()
+    @pytest.fixture(scope="class",
+                    params = [(load_benzene, ('Coulomb', 'VDW'), (5, 16)),
+                              ])
+    def dataset(self, request):
+        '''The input dataset is specified as:
+        load_method: The method for loading the dataset.
+        keys: The keys to the corresponding dataset.
+        value_length: The expected number of file for each key.
+        '''
+        load_method, keys, value_length = request.param
+        return load_method, keys, value_length
 
-    def test_keys(self, bz):
-        assert len(bz.data.keys()) == 2
+    def test_num_files(self, dataset):
+        '''Test if the number of files matches with the expected number of
+        files.'''
+        load_method, keys, value_length = dataset
+        for index, key in enumerate(keys):
+            assert len(load_method().data[key]) == value_length[index]
 
-    def test_coulomb(self, bz):
-        assert len(bz.data['Coulomb']) == 5
-
-    def test_vdw(self, bz):
-        assert len(bz.data['VDW']) == 16
+    def test_file_exist(self, dataset):
+        '''Test if files do exist.'''
+        load_method, keys, value_length = dataset
+        for key in keys:
+            for file in load_method().data[key]:
+                if not os.path.isfile(file):
+                    raise FileNotFoundError('No such file: {}'.format(file))
