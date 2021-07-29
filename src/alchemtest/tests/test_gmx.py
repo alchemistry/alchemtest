746a33f757b67fc05324bc2fa9e1a9af8535f414
Oliver Beckstein <orbeckst@gmail.com>
2021-07-29 12:51:47 -0700

    test all GMX datasets
    close #51

--- src/alchemtest/tests/test_gmx.py
+++ src/alchemtest/tests/test_gmx.py
@@ -1,13 +1,30 @@
 '''Tests for all the gromacs dataset'''
 import pytest
 
-from alchemtest.gmx import load_benzene
+from alchemtest.gmx import (load_benzene, load_ABFE,
+                            load_expanded_ensemble_case_1,
+                            load_expanded_ensemble_case_2,
+                            load_expanded_ensemble_case_3,
+                            load_water_particle_without_energy,
+                            load_water_particle_with_potential_energy,
+                            load_water_particle_with_total_energy,
+                            )
 
 from . import BaseDatasetTest
 
-class TestBenzene(BaseDatasetTest):
+
+class TestGROMACS(BaseDatasetTest):
     @pytest.fixture(scope="class",
                     params = [(load_benzene, ('Coulomb', 'VDW'), (5, 16)),
+                              (load_ABFE, ('complex', 'ligand'), (30, 20)),
+                              (load_expanded_ensemble_case_1, ('AllStates', ), (1,)),
+                              (load_expanded_ensemble_case_2, ('AllStates', ), (2,)),
+                              (load_expanded_ensemble_case_3, ('AllStates', ), (32,)),
+                              (load_water_particle_without_energy, ('AllStates', ), (38,)),
+                              (load_water_particle_with_potential_energy, ('AllStates', ), (38,)),
+                              (load_water_particle_with_total_energy, ('AllStates', ), (38,)),
                               ])
     def dataset(self, request):
-        return super().dataset(request)
+        return super(TestGROMACS, self).dataset(request)
+
+
