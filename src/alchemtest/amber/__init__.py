84fca66ee54e6cf1a0759dc53f5419733e14c14e
DrDomenicoMarson <domenico87@gmail.com>
2022-09-14 08:20:32 +0200

    removed unnecessary load_single... funcions

--- src/alchemtest/amber/__init__.py
+++ src/alchemtest/amber/__init__.py
@@ -6,5 +6,3 @@ from .access import load_bace_improper
 from .access import load_bace_example
 from .access import load_simplesolvated
 from .access import load_invalidfiles
-from .access import load_single_u_nk_file
-from .access import load_single_dHdl_file
