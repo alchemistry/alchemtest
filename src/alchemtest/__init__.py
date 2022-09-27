edbdb294b513403a7cce9ce005b789b9f0f44fcd
Oliver Beckstein <orbeckst@gmail.com>
2022-09-27 13:30:33 -0700

    updated versioneer files
    - only compatible with Python 3.7+
    - used versioneer 0.26
    - still uses vendor mode

--- src/alchemtest/__init__.py
+++ src/alchemtest/__init__.py
@@ -1,7 +1,6 @@
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 class Bunch(dict):
     """Container object for datasets
