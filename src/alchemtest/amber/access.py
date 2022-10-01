7add57ea3b4628b364b4d3fb39344635ff94c2b0
DrDomenicoMarson <domenico87@gmail.com>
2022-10-01 15:41:32 +0200

    got rid of (glob, join, dirname) using Path

--- src/alchemtest/amber/access.py
+++ src/alchemtest/amber/access.py
@@ -2,9 +2,7 @@
 
 """
 
-from os.path import dirname, join
 from os import listdir
-from glob import glob
 from pathlib import Path
 from .. import Bunch
 
@@ -21,10 +19,10 @@ def load_bace_improper():
         - 'data' : the data files for improper solvated vdw alchemical leg
 
     """
-    module_path = dirname(__file__)
-    data = {'vdw': glob(join(module_path, 'bace_improper/solvated/vdw/*/ti-*.out.bz2'))}
+    module_path = Path(__file__).parent
+    data = {'vdw': list(map(str, module_path.glob('bace_improper/solvated/vdw/*/ti-*.out.bz2')))}
 
-    with open(join(module_path, 'bace_improper', 'descr.rst')) as rst_file:
+    with open(module_path / 'bace_improper' / 'descr.rst') as rst_file:
         fdescr = rst_file.read()
 
     return Bunch(data=data,
@@ -42,20 +40,20 @@ def load_bace_example():
         - 'data' : the data files by system and alchemical leg
 
     """
-    module_path = dirname(__file__)
+    module_path = Path(__file__).parent
     data = {'complex':
-                {'decharge': glob(join(module_path, 'bace_CAT-13d~CAT-17a/complex/decharge/*/ti-*.out.bz2')),
-                 'recharge': glob(join(module_path, 'bace_CAT-13d~CAT-17a/complex/recharge/*/ti-*.out.bz2')),
-                 'vdw': glob(join(module_path, 'bace_CAT-13d~CAT-17a/complex/vdw/*/ti-*.out.bz2'))
+                {'decharge': list(map(str, module_path.glob('bace_CAT-13d~CAT-17a/complex/decharge/*/ti-*.out.bz2'))),
+                 'recharge': list(map(str, module_path.glob('bace_CAT-13d~CAT-17a/complex/recharge/*/ti-*.out.bz2'))),
+                 'vdw': list(map(str, module_path.glob('bace_CAT-13d~CAT-17a/complex/vdw/*/ti-*.out.bz2')))
                  },
             'solvated':
-                {'decharge': glob(join(module_path, 'bace_CAT-13d~CAT-17a/solvated/decharge/*/ti-*.out.bz2')),
-                 'recharge': glob(join(module_path, 'bace_CAT-13d~CAT-17a/solvated/recharge/*/ti-*.out.bz2')),
-                 'vdw': glob(join(module_path, 'bace_CAT-13d~CAT-17a/solvated/vdw/*/ti-*.out.bz2'))
+                {'decharge': list(map(str, module_path.glob('bace_CAT-13d~CAT-17a/solvated/decharge/*/ti-*.out.bz2'))),
+                 'recharge': list(map(str, module_path.glob('bace_CAT-13d~CAT-17a/solvated/recharge/*/ti-*.out.bz2'))),
+                 'vdw': list(map(str, module_path.glob('bace_CAT-13d~CAT-17a/solvated/vdw/*/ti-*.out.bz2')))
                  }
             }
 
-    with open(join(module_path, 'bace_CAT-13d~CAT-17a', 'descr.rst')) as rst_file:
+    with open(module_path / 'bace_CAT-13d~CAT-17a' / 'descr.rst') as rst_file:
         fdescr = rst_file.read()
 
     return Bunch(data=data,
@@ -75,11 +73,11 @@ def load_simplesolvated():
 
     """
 
-    module_path = dirname(__file__)
-    data = {'charge': glob(join(module_path, 'simplesolvated/charge/*/ti-*.tar.bz2')),
-            'vdw': glob(join(module_path, 'simplesolvated/vdw/*/ti-*.tar.bz2'))}
+    module_path = Path(__file__).parent
+    data = {'charge': list(map(str, module_path.glob('simplesolvated/charge/*/ti-*.tar.bz2'))),
+            'vdw': list(map(str, module_path.glob('simplesolvated/vdw/*/ti-*.tar.bz2')))}
 
-    with open(join(module_path, 'simplesolvated', 'descr.rst')) as rst_file:
+    with open(module_path / 'simplesolvated' / 'descr.rst') as rst_file:
         fdescr = rst_file.read()
 
     return Bunch(data=data,
@@ -108,16 +106,16 @@ def load_invalidfiles():
         "load_invalidfiles() was deprecated in 0.7.0 and will be removed in the following release."
         " Use load_testfiles() instead",
         DeprecationWarning)
-    module_path = dirname(__file__)
+    module_path = Path(__file__).parent
     data = [[
-        glob(join(module_path, 'testfiles', 'no_useful_data.out.tar.bz2'))[0],
-        glob(join(module_path, 'testfiles', 'no_control_data.out.tar.bz2'))[0],
-        glob(join(module_path, 'testfiles', 'no_temp0_setted.out.tar.bz2'))[0],
-        glob(join(module_path, 'testfiles', 'no_free_energy_info.out.tar.bz2'))[0],
-        glob(join(module_path, 'testfiles', 'no_atomic_section.out.tar.bz2'))[0],
-        glob(join(module_path, 'testfiles', 'no_results_section.out.tar.bz2'))[0]]]
+        module_path / 'testfiles' / 'no_useful_data.out.tar.bz2',
+        module_path / 'testfiles' / 'no_control_data.out.tar.bz2',
+        module_path / 'testfiles' / 'no_temp0_setted.out.tar.bz2',
+        module_path / 'testfiles' / 'no_free_energy_info.out.tar.bz2',
+        module_path / 'testfiles' / 'no_atomic_section.out.tar.bz2',
+        module_path / 'testfiles' / 'no_results_section.out.tar.bz2']]
 
-    with open(join(module_path, 'testfiles', 'descr_invalid.rst')) as rst_file:
+    with open(module_path / 'testfiles' / 'descr_invalid.rst') as rst_file:
         fdescr = rst_file.read()
 
     return Bunch(data=data,
@@ -137,17 +135,17 @@ def load_testfiles():
 
     """
 
-    module_path = dirname(__file__)
+    module_path = Path(__file__).parent
 
     data = {}
-    for f in listdir(join(module_path, 'testfiles')):
+    for f in listdir(module_path / 'testfiles'):
         f_path = Path(f)
         if f_path.suffix==".bz2":
             while f_path.suffix in ('.tar', '.bz2', '.out'):
                 f_path = f_path.with_suffix('')
-            data[f_path.name] = [join(module_path, 'testfiles', f)]
+            data[f_path.name] = [module_path / 'testfiles' / f]
 
-    with open(join(module_path, 'testfiles', 'descr.rst')) as rst_file:
+    with open(module_path / 'testfiles' / 'descr.rst') as rst_file:
         fdescr = rst_file.read()
 
     return Bunch(data=data,
